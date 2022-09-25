987a14c388439f0399be78eb0718666c72f655d0
mohand-zaid <muhanadaleem@gmail.com>
2022-09-26 00:39:42 +0200

    Setting up data structure and database handler

---
+++ database.py
@@ -0,0 +1,33 @@
+import json
+
+class DBHandler :
+    
+    # When initiating the object means to make new profile or edit a profile
+    def __init__(self, username, master_password, email=None) :
+        
+        self.username = username
+        self.password = master_password
+        self.email = email
+
+        profiles_buffer = self.load_db('profiles.json')
+
+        profiles_buffer.update({self.username : { 'email': self.email, 'password': self.password}})
+
+        self.save_db(profiles_buffer, 'profiles.json')
+
+
+        
+    def load_db(self, db_name):
+        try:
+            with open(f'db/{db_name}', 'r') as db:
+                return json.load(db)
+        except FileNotFoundError as err :
+            return err
+
+    def save_db(self, db_buffer_to_save, db_name):
+        try:
+            with open(f'db/{db_name}', 'w') as db:
+                json.dump(db_buffer_to_save, db)
+        except NameError as err :
+            return err
+
